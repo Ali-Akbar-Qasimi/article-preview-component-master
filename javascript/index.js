293221c79fc3a31a4a17bf158367cc3a6c0eb5a1
Ali Akbar Qaismi <aliaqasimi90@gmail.com>
2022-04-14 13:51:25 +0800

    deploying my project

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+let share = document.querySelector('.share')
+// let hidden = document.querySelector('.hidden')
+
+
+
+share.addEventListener('click',()=>{
+//     hidden.style.visibility = 'visible'
+//     hidden.style.opacity = '1'
+share.classList.toggle('open')
+})
